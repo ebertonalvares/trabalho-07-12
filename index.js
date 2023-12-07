4db6a6b69f23ce531c01680465debf91e3b64a7e
eberton mauricio alvares <ebertonalvares@hotmail.com>
2023-12-07 20:20:36 -0300

    entrada de dados

---
+++ index.js
@@ -0,0 +1,12 @@
+const readline = require("readline-sync")
+// calculo de media de notas de um bimestre 
+//ENTRADA
+//O usuario deve informar 4 notas
+let nota_prova_1 = readline.questionFloat("Qual a nota da prova 1?\n");
+let nota_prova_2 = readline.questionFloat("Qual a nota da prova 2?\n");
+let nota_prova_3 = readline.questionFloat("Qual a nota da prova 3?\n");
+let nota_prova_4 = readline.questionFloat("Qual a nota da prova 4?\n");
+console.log(nota_prova_1, nota_prova_2, nota_prova_3, nota_prova_4);
+//PROCESSAMENTO 
+
+//SAIDA
